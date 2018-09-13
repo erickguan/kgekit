d91ad03c50c1f3b7441160c4203be022ff54858f
Erick Guan <fantasticfears@gmail.com>
2018-09-13 23:22:40 +0200

    Fix the build

--- kgekit.cpp
+++ kgekit.cpp
@@ -1,13 +1,17 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "kgekit.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(kgecpp, m) {
-    m.doc() = "kgecpp helps with io and sampling"; // optional module docstring
+PYBIND11_MODULE(kgekit, m) {
+    m.doc() = "kgekit helps with io and sampling"; // optional module docstring
     m.def("read_triple_index", &kgekit::read_triple_index, "read dataset file includes triple index.",
+          py::arg("filename"),
           py::arg("triple_order") = "hrt",
           py::arg("sperator") = '\t');
     m.def("get_triple_index", &kgekit::get_triple_index, "get triple index from a line",
-        py::arg("sperator") = '\t');
+          py::arg("line"),
+          py::arg("order"),
+          py::arg("sperator"));
 }
