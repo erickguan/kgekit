a045f12ab03e885311a954ddaf40446593c78513
Erick Guan <fantasticfears@gmail.com>
2018-09-20 21:51:48 +0200

    this also didn't hold value

--- kgekit_impl_test.cpp
+++ kgekit_impl_test.cpp
@@ -66,7 +66,7 @@ TEST_CASE("get_triple_index", "[parsing]")
     REQUIRE_THAT(get_triple_index("1\t3\t2", "htr", '\t'),
                  Predicate<optional<TripleIndex>>([] (const auto& t) -> bool { return t->head == 1 && t->relation == 2 && t->tail == 3; }, "gets the triple"));
     REQUIRE_THAT(get_triple_index("1\t2\t3", "hrt", ' '), // can be replaced by t.has_value()
-                 Predicate<optional<TripleIndex>>([] (const auto& t) -> bool { if (t) { return true; } else { return false; } }, "return null when wrong separator doesn't return value"));
+                 Predicate<optional<TripleIndex>>([] (const auto& t) -> bool { if (t) { return false; } else { return true; } }, "return null when wrong separator doesn't return value"));
     REQUIRE_THAT(get_triple_index("1\t2\t3", "htt", '\t'),
                  Predicate<optional<TripleIndex>>([] (const auto& t) -> bool { if (t) { return false; } else { return true; } }, "return null when wrong order for the triple index"));
     REQUIRE_THAT(get_triple_index("1\t3", "hrt", '\t'),
