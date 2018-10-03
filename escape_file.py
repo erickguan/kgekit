067f54ffda80b9a7dd50a155ee1083d49d0e212e
Erick Guan <fantasticfears@gmail.com>
2018-10-03 16:51:31 +0200

    fix escape script

--- escape_file.py
+++ escape_file.py
@@ -4,9 +4,9 @@ import os
 
 def escape_file(path):
     paths, filename = os.path.split(path)
-    with open(path, 'r') as f1, open(os.path.join(paths, "escaped"+filename), 'w') as f2:
+    with open(path, 'r') as f1, open(os.path.join(paths, "escaped_"+filename), 'w') as f2:
         content = f1.read()
-        escaped = re.sub(r'\\u00(\d+)', '%\1', content)
+        escaped = re.sub(r'\\u00(\d+)', r'%\1', content)
         f2.write(escaped)
 
 escape_file(sys.argv[1])
