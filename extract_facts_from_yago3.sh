77bc7b9a0464ee43d0480f93b36f215c1709851b
Erick Guan <fantasticfears@gmail.com>
2018-10-04 09:15:18 +0200

    use distribution files

--- extract_facts_from_yago3.sh
+++ extract_facts_from_yago3.sh
@@ -1,1 +1,6 @@
-./gradlew run --info --args="-action extract -triple_file ../../yagodump/escaped_yagoFacts.ttl -triple_output=../triples.txt"
+./gradlew distTar
+mkdir tmp
+mv driver/build/distributions/driver.tar tmp/driver.tar
+cd tmp
+tar vxf driver.tar
+./bin/driver -action extractTriple -triple_file ../../../yagodump/escaped_yagoFacts.ttl -triple_output=../triples.txt"
