6f74344d6be2a673b6aca2810b3d1f9f6a7112fa
Erick Guan <fantasticfears@gmail.com>
2018-10-12 13:56:58 +0200

    checking reverse pairs

--- reverse_triples_validation.py
+++ reverse_triples_validation.py
@@ -1,10 +1,21 @@
 import sys
-import kgekit
+import kgekit.io
 
 def validate(triples):
-    kgekit.
-    pass
+    indexer = kgekit.EntityNumberIndexer(triples, "hrt")
+    indexes = indexer.indexes()
+    mapping = {}
+    for t in indexes:
+        h, r, t = t.head, t.relation, t.tail
+        if h < t:
+            k = (h,t)
+        else:
+            k = (t,h)
+        mapping.setdefault(k, list())
+        mapping[k].append(r)
+    for k,v in mapping.items():
+        if len(v) > 1:
+            print(k,v)
 
 if __name__ == '__main__':
-    with open(sys.argv[1]) as f:
-        validate(f.read())
+    validate(kgekit.io.read_triples(sys.argv[1], "hrt", " "))
