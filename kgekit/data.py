c76be2840e9784ad5615869ae3f082c6e982a19a
Erick Guan <fantasticfears@gmail.com>
2018-10-18 16:54:08 +0200

    unpack functions

--- kgekit/data.py
+++ kgekit/data.py
@@ -1,5 +1,5 @@
 """data utils function"""
 
-def unwrap(triple):
-    """unwraps triple into (h, r, t). Can take Triple or TripleIndex."""
+def unpack(triple):
+    """unpacks triple into (h, r, t). Can take Triple or TripleIndex."""
     return triple.head, triple.relation, triple.tail
--- kgekit/tests/data_test.py
+++ kgekit/tests/data_test.py
@@ -3,8 +3,8 @@ import kgekit.data
 
 class DataTest(unittest.TestCase):
     def test_unwrap(self):
-        self.assertEqual(kgekit.data.unwrap(kgekit.TripleIndex(1, 2, 3)), (1, 2, 3))
-        self.assertEqual(kgekit.data.unwrap(kgekit.Triple("/m/1", "/m/2", "/m/3")), ("/m/1", "/m/2", "/m/3"))
+        self.assertEqual(kgekit.data.unpack(kgekit.TripleIndex(1, 2, 3)), (1, 2, 3))
+        self.assertEqual(kgekit.data.unpack(kgekit.Triple("/m/1", "/m/2", "/m/3")), ("/m/1", "/m/2", "/m/3"))
 
 if __name__ == '__main__':
     unittest.main()
