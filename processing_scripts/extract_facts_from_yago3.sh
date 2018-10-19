404f8c5f6ed59fe4becf2341571a3d40d5499766
Erick Guan <fantasticfears@gmail.com>
2018-10-19 20:16:39 +0200

    fix scripts permissions

--- processing_scripts/extract_facts_from_yago3.sh
+++ processing_scripts/extract_facts_from_yago3.sh
@@ -1,3 +1,3 @@
 ./gradlew run \
---args="extractTriple --triple_file=../yagodump/escaped_yagoFacts.ttl --output_file=tmp/triples.txt" \
+--args="extractTriple --triple_file=../../yagodump/escaped_yagoFacts.ttl --output_file=../tmp/triples.txt" \
 --info
--- processing_scripts/extract_labels_from_yago3.sh
+++ processing_scripts/extract_labels_from_yago3.sh
@@ -1,1 +1,3 @@
-../gradlew run --args="extractLabel --triple_file=../../../yagodump/escaped_yagoFacts.ttl --label_file=escaped_yagoLabels.ttl --output_file=../labels.txt"
+./gradlew run \
+--args="extractLabel --triple_file=../../yagodump/escaped_yagoFacts.ttl --label_file=escaped_yagoLabels.ttl --output_file=../tmp/labels.txt" \
+--info
