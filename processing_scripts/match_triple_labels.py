e3ee93ffc9c95339740f625f76f742cdc63128c7
Erick Guan <fantasticfears@gmail.com>
2018-10-26 14:56:29 +0200

    wrong parameters

--- processing_scripts/match_triple_labels.py
+++ processing_scripts/match_triple_labels.py
@@ -5,16 +5,16 @@ import kgekit.data
 import random
 from itertools import chain, dropwhile
 
-def match_triple_labels(triple_filename, label_filename):
+def match_triple_labels(triple_filename):
     triples, _ = kgekit.io.read_triples(triple_filename, 'hrt', ' ')
     chained_triple = chain([(t.head, t.tail) for t in triples])
     entities = frozenset(chained_triple)
-    labels = kgekit.io.read_labels(label_filename, ' ')
+    labels = kgekit.io.read_labels(triple_filename, ' ')
     filtered_labels = list(dropwhile(lambda x: x[0] in entities, labels))
     return filtered_labels
 
 if __name__ == '__main__':
-    labels = match_triple_labels(sys.argv[1], sys.argv[2])
+    labels = match_triple_labels(sys.argv[1])
     with open('matched_labels.txt', 'w') as f:
         for l in labels:
             f.write(" ".join(l) + "\n")
