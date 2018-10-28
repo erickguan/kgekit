3377e75176de077e006a36ab760e56d6535ea3cb
Erick Guan <fantasticfears@gmail.com>
2018-10-28 12:29:51 +0100

    add shuffle and split sets

--- kgekit/data.py
+++ kgekit/data.py
@@ -1,5 +1,26 @@
 """data utils function"""
 
+import numpy as np
+
 def unpack(triple):
     """unpacks triple into (h, r, t). Can take Triple or TripleIndex."""
     return triple.head, triple.relation, triple.tail
+
+def shuffle_triples(triples):
+    """Shuffle triples."""
+    return np.random.permutation(triples)
+
+def split_golden_set(triples, valid_ratio, test_ratio):
+    """Split the data into train/valid/test sets."""
+    assert valid_ratio >= 0.0
+    assert test_ratio >= 0.0
+    num_valid = len(triples) * valid_ratio
+    num_test = len(triples) * test_ratio
+    valid_set = triples[:num_valid]
+    test_set = triples[num_valid:num_valid+num_test]
+    train_set = triples[num_valid+num_test:]
+    assert len(valid_set) + len(test_set) + len(train_set) == len(triples)
+
+    return train_set, valid_set, test_set
+
+
---
+++ processing_scripts/shuffle_split_gold_sets.py
@@ -0,0 +1,20 @@
+"""Shuffle and split the data into train/valid/test sets."""
+
+import sys
+import kgekit.data
+import kgekit.io
+
+def shuffled_split(filename):
+    triples, num_failed = kgekit.io.read_triples(filename, 'hrt', ' ')
+    if num_failed > 0:
+        print("There is " + str(num_failed) + " failures.")
+    indexer = kgekit.EntityNumberIndexer(triples, "hrt")
+    indexes = indexer.indexes()
+    indexes = kgekit.data.shuffle_triples(indexes)
+    train, valid, test = kgekit.data.split_golden_set(indexes, 0.005, 0.005)
+    kgekit.io.write_triples('train.txt', train)
+    kgekit.io.write_triples('valid.txt', valid)
+    kgekit.io.write_triples('test.txt', test)
+
+if __name__ == '__main__':
+    shuffled_split(sys.argv[1])
