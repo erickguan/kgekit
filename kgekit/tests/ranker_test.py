c07a1c0375cb42a82ff854e14bd5d370f84dbfc9
Erick Guan <fantasticfears@gmail.com>
2018-11-20 23:23:32 +0100

    a little more test

--- kgekit/tests/ranker_test.py
+++ kgekit/tests/ranker_test.py
@@ -21,16 +21,31 @@ class RankerTest(unittest.TestCase):
         ], dtype=np.float32), kgekit.TripleIndex(4, 2, 3))
         self.assertEqual(ranks, (5, 4))
 
+        ranks = self.ranker.rankHead(np.array([
+            0.998, 0.032, 0.003, 0.184, 0.265, 0.356, 0.237
+        ], dtype=np.float32), kgekit.TripleIndex(5, 2, 3))
+        self.assertEqual(ranks, (6, 4))
+
     def test_rank_tail(self):
         ranks = self.ranker.rankTail(np.array([
             0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007
         ], dtype=np.float32), kgekit.TripleIndex(4, 2, 3))
         self.assertEqual(ranks, (4, 4))
 
+        ranks = self.ranker.rankTail(np.array([
+            0.998, 0.032, 0.003, 0.184, 0.265, 0.356, 0.237
+        ], dtype=np.float32), kgekit.TripleIndex(1, 2, 4))
+        self.assertEqual(ranks, (5, 4))
+
     def test_rank_relation(self):
         ranks = self.ranker.rankRelation(np.array([
             0.001, 0.002, 0.003, 0.004
         ], dtype=np.float32), kgekit.TripleIndex(4, 2, 3))
+        self.assertEqual(ranks, (3, 2))
+
+        ranks = self.ranker.rankRelation(np.array([
+            0.231, 0.562, 0.923, 0.344
+        ], dtype=np.float32), kgekit.TripleIndex(3, 1, 5))
         self.assertEqual(ranks, (3, 2))
 
 if __name__ == '__main__':
