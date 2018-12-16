31bbe36489e36211db445a90ff4d7c83639f4402
Erick Guan <fantasticfears@gmail.com>
2018-12-16 21:38:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ class CleanCommand(Command):
 
 
 setup(name='kgekit',
-      version='0.3.5',
+      version='0.4.0',
       description='knowledge representation tools',
       long_description=open("README.md", "r").read(),
       long_description_content_type="text/markdown",
