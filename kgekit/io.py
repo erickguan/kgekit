72ae2d777d3f5e9ab6bbab3365c5c226fe1b0c2b
Erick Guan <fantasticfears@gmail.com>
2018-12-18 04:55:36 +0100

    fix a missing parameter

--- kgekit/io.py
+++ kgekit/io.py
@@ -56,11 +56,11 @@ def write_index_translation(translation_filename, entity_ids, relation_ids, deli
     with open(translation_filename, "wb") as f:
         f.write(translation.SerializeToString())
 
-def write_triples(filename, triples, delimiter=DEFAULT_DELIMITER):
+def write_triples(filename, triples, delimiter=DEFAULT_DELIMITER, triple_order="hrt"):
     """write triples to file."""
     with open(filename, 'w') as f:
         for t in triples:
-            line = t.serialize(delimiter)
+            line = t.serialize(delimiter, triple_order)
             f.write(line + "\n")
 
 def read_translation(filename):
