d652298506bbf8b7322e4a8678fd4f2aed72901a
Erick Guan <fantasticfears@gmail.com>
2019-01-21 16:06:44 +0100

    add build dataset script

--- processing_scripts/build_dataset.py
+++ processing_scripts/build_dataset.py
@@ -4,7 +4,7 @@ import sys
 import kgekit.data
 import kgekit.io
 
-def shuffled_split(filename):
+def build_dataset(filename, deficit_threshold=0.0000124):
     triples, num_failed = kgekit.io.read_triples(filename, 'hrt', ' ')
     if num_failed > 0:
         print("There is " + str(num_failed) + " failures.")
@@ -12,10 +12,16 @@ def shuffled_split(filename):
     indexes = indexer.indexes()
     kgekit.io.write_index_translation('translation.protobuf', indexer.entityIdMap(), indexer.relationIdMap())
     indexes = kgekit.data.shuffle_triples(indexes)
-    train, valid, test = kgekit.data.split_golden_set(indexes, 0.005, 0.005)
+    train, valid, test = kgekit.data.build_dataset(
+        triples,
+        0.005,
+        0.005,
+        deficit_threshold=deficit_threshold,
+        duplicate_threshold=0.97,
+        inverse_threshold=0.97)
     kgekit.io.write_triples('train.txt', train)
     kgekit.io.write_triples('valid.txt', valid)
     kgekit.io.write_triples('test.txt', test)
 
 if __name__ == '__main__':
-    shuffled_split(sys.argv[1])
+    build_dataset(sys.argv[1], float(sys.argv[2]))
