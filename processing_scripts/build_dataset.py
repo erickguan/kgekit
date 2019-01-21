878e3e1e5d031f94d7a7a57ea734e5b7861ddbda
Erick Guan <fantasticfears@gmail.com>
2019-01-21 16:13:36 +0100

    fix import name in build_dataset

--- processing_scripts/build_dataset.py
+++ processing_scripts/build_dataset.py
@@ -3,12 +3,13 @@
 import sys
 import kgekit.data
 import kgekit.io
+import kgedata
 
 def build_dataset(filename, deficit_threshold=0.0000124, delimiter=' ', triple_order="hrt"):
     triples, num_failed = kgekit.io.read_triples(filename, triple_order=triple_order, delimiter=delimiter)
     if num_failed > 0:
         print("There is " + str(num_failed) + " failures.")
-    indexer = kgekit.EntityNumberIndexer(triples, "hrt")
+    indexer = kgedata.EntityNumberIndexer(triples, "hrt")
     indexes = indexer.indexes()
     kgekit.io.write_index_translation('translation.protobuf', indexer.entityIdMap(), indexer.relationIdMap())
     indexes = kgekit.data.shuffle_triples(indexes)
