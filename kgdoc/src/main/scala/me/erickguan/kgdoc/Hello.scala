891333f04674a69f27aae957bd064519b78f6b89
Erick Guan <fantasticfears@gmail.com>
2019-02-20 14:39:26 +0100

    add template for kgdoc

---
+++ kgdoc/src/main/scala/me/erickguan/kgdoc/Hello.scala
@@ -0,0 +1,9 @@
+package me.erickguan.kgdoc
+
+object Hello extends Greeting with App {
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
---
+++ kgdoc/src/test/scala/me/erickguan/kgdoc/HelloSpec.scala
@@ -0,0 +1,9 @@
+package me.erickguan.kgdoc
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
