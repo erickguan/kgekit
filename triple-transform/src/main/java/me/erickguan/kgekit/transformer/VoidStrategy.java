9fa1157f09fd2bd0f2c58403960252432b20a3ba
Erick <fantasticfears@gmail.com>
2019-03-25 09:45:45 +0100

    drop java module; the functionality has been moved to kgdoc

--- triple-transform/src/main/java/me/erickguan/kgekit/transformer/VoidStrategy.java
+++
@@ -1,9 +0,0 @@
-package me.erickguan.kgekit.transformer;
-
-import java.io.IOException;
-
-public class VoidStrategy extends TransformStrategy {
-    public void transform(Transformer transformer) throws IOException {
-        return;
-    }
-}
--- triple-transform/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module me.erickguan.kgekit.transition {
-    exports me.erickguan.kgekit.transformer;
-    exports me.erickguan.kgekit.writer;
-    requires org.slf4j;
-    requires rdf4j.rio.api;
-    requires rdf4j.rio.turtle;
-    requires rdf4j.model;
-    requires rdf4j.util;
-}
