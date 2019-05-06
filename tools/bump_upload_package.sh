0ee92a4871f9547fd683f76419f1f686bb85e114
Erick Guan <fantasticfears@gmail.com>
2019-05-06 19:22:38 +0200

    generate bdist too

--- tools/bump_upload_package.sh
+++ tools/bump_upload_package.sh
@@ -5,5 +5,5 @@ echo "Entering $DIR for uploading new package version $VERSION"
 cd $DIR
 git add kgekit/__init__.py
 git commit -m "version bump"
-python3 setup.py sdist
+python3 setup.py sdist bdist_wheel
 twine upload --verbose -s -i 76407351 -u fantasticfears dist/kgekit-$VERSION.tar.gz
